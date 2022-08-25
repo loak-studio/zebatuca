3b49063c465399a782d6c0a8e55ded88372f9d93
benjamingeets <benjamingeets@tutamail.com>
2022-08-25 12:21:57 +0200

    feat: header

---
+++ src/scripts/hamburger-menu.js
@@ -0,0 +1,18 @@
+window.addEventListener("load", () => {
+  const button = document.querySelector("[toggle-nav]");
+  const nav = document.querySelector("[main-nav]");
+  const body = document.querySelector("body");
+  const hamburger = document.querySelector("#hamburger");
+  button.addEventListener("click", () => {
+    nav.classList.toggle("scale-y-0");
+    nav.classList.toggle("opacity-0");
+    window.scrollTo(0, 0);
+    body.classList.toggle("overflow-hidden");
+    hamburger.classList.toggle("pt-3");
+    hamburger.children[0].classList.toggle("rotate-45");
+    hamburger.children[0].classList.toggle("absolute");
+    hamburger.children[1].classList.toggle("opacity-0");
+    hamburger.children[2].classList.toggle("-rotate-45");
+    hamburger.children[2].classList.toggle("absolute");
+  });
+});
