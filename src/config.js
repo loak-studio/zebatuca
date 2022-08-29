590c3a700033f0552247b2606e8873f90475fa90
benjamingeets <benjamingeets@tutamail.com>
2022-08-29 14:56:37 +0200

    feat: social links

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
 export const config = {
   api: "http://localhost:1337",
+  facebook: "https://www.facebook.com/people/Zebatuca/100046798485449/",
+  instagram: "https://www.instagram.com/zebatucaconcerts/",
 };
