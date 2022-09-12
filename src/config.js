942c4e91b6e7c13463c29b6fc5d2c6a51119825d
benjamingeets <benjamingeets@tutamail.com>
2022-09-12 15:13:08 +0200

    fix(config): remove slash

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const config = {
-  api: "https://strapi-zebatuca.loak.studio/",
+  api: "https://strapi-zebatuca.loak.studio",
   facebook: "https://www.facebook.com/people/Zebatuca/100046798485449/",
   instagram: "https://www.instagram.com/zebatucaconcerts/",
 };
