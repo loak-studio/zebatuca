cc450332a02d3fa5864f6c9b722fc76351cfe68c
benjamingeets <benjamingeets@tutamail.com>
2022-09-12 14:42:19 +0200

    feat: add API url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const config = {
-  api: "http://localhost:1337",
+  api: "https://strapi-zebatuca.loak.studio/",
   facebook: "https://www.facebook.com/people/Zebatuca/100046798485449/",
   instagram: "https://www.instagram.com/zebatucaconcerts/",
 };
